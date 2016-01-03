417942eafdcfecbe6257ed7844c310521668f494
duiliopastorelli <mezgec.danilo@gmail.com>
2016-01-03 21:13:13 +0000

    Index page working with single and all pages queries

--- index.php
+++ index.php
@@ -3,8 +3,27 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/src/config.php';
 
-echo "Hello World!";
+// Make available the $db to other classes
+$db = new SmartBear\DB(HOST, USER, PASSWORD, DATABASE);
 
 $p = new SmartBear\Address();
-echo $p->getAddress($_GET);
+$pageData = $p->getAddress($_GET);
+?><pre><?php print_r($pageData);?></pre>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+<body>
+    <?php foreach ($pageData as $data) { ?>
+        <div class="post">
+            <h2><?php echo $data->post_title; ?></h2>
+            <p><?php echo $data->post_content; ?></p>
+        </div>
+        <?php } ?>
+    </body>
+    </html>
--- src/Address.php
+++ src/Address.php
@@ -7,17 +7,26 @@ class Address
     // Get the address
     public function getAddress($address)
     {
+        // Instance the select class to retrieve the posts from the db
+        $select = new DbSelect;
+
         if (!empty ($address['p'])) {
             $post = $address['p'];
         };
+
         if (!empty ($address['cat'])) {
             $cat = $address['cat'];
         }
 
+        // Show the home page (list of all the posts)
         if (empty ($post) && empty ($cat)){
-            return "home";
+            return $select->allPosts();
+
+        // Show the single post
         } elseif (!empty ($post)) {
-            return "single";
+            return $select->post($post);
+
+        // Show the category page
         } elseif (!empty ($cat)) {
             return "cat";
         }
